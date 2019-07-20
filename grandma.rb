49919ae0e0e3b62d8c922533e6b9f4f09d6f7518
Jarred Wilkins <wilkins.jarred@outlook.com>
2019-07-20 00:46:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
   if phrase=="Hi, Nana how are you?".upcase
      "HUH? SPEAK UP, SONNY!"
   elsif phrase=="WHAT DID YOU EAT TODAY?"
-     "NO, NOT SINCE 1938"
+     "NO, NOT SINCE 1938!"
   else phrase=="I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
   end
